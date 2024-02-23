3c6d5d7238da506bf01017ed69382aa5980a20b2
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-02-22 21:38:15 -0800

    Implement playRound function

--- javascript.js
+++ javascript.js
@@ -8,3 +8,34 @@ function getComputerChoice() {
     return 'Scissors';
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == "rock") {
+    if (computerSelection == "Scissors") {
+      return "You win! Rock beats Scissors";
+    } else if (computerSelection == "Paper") {
+      return "You lose! Paper beats Rock";
+    } else {
+      return "It's a draw! You both picked Rock";
+    }
+  }
+  if (playerSelection == "paper") {
+    if (computerSelection == "Rock") {
+      return "You win! Paper beats Rock";
+    } else if (computerSelection == "Scissors") {
+      return "You lose! Scissors beats Paper";
+    } else {
+      return "It's a draw! You both picked Paper";
+    }
+  }
+  if (playerSelection == "scissors") {
+    if (computerSelection == "Paper") {
+      return "You win! Scissors beats Paper";
+    } else if (computerSelection == "Rock") {
+      return "You lose! Rock beats Scissors";
+    } else {
+      return "It's a draw! You both picked Scissors";
+    }
+  }
+}
